3a5322c7dea42e9e97f5b0375c5f1ee88306958f
abrokenlife <yrom11@yandex.ru>
2019-08-08 01:26:38 +0300

    2

--- settings.py
+++
@@ -1,4 +0,0 @@
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
-API_KEY = '632006628:AAG9wyctEzEAF25XbNUDPUaNJ87UYxlXris'
